119ea2d4255f9f1b8d873ad38257b628788671c9
abiramen <7523422+abiramen@users.noreply.github.com>
2023-06-05 08:41:26 +1000

    improve starter code

---
+++ week02-m12c/square.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int x;
+    int y;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    y = x * x;
+    printf("%d\n", y);
+    return 0;
+}
---
+++ week02-m12c/square.s
@@ -0,0 +1,9 @@
+main:
+	# Locals:
+	# - $
+
+
+	li	$v0, 0		#
+	jr	$ra		# return 0;
+
+	.data
