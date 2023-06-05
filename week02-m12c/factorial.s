ab74322f74a0cd3d49aa78112bf10783f71bea1d
abiramen <7523422+abiramen@users.noreply.github.com>
2023-06-05 13:30:51 +1000

    week02-m12c: add completed factorial code in MIPS

--- week02-m12c/factorial.s
+++ week02-m12c/factorial.s
@@ -1,34 +1,52 @@
-# Iteratively calculate a factorial.
-
 main:
-	# Locals:
+        # Locals:
 	# - $t0: int n
+	# - $t1: int fac
+	# - $t2: int i
 
 	li	$v0, 4			# syscall 4: print_string
 	la	$a0, prompt_msg		#
-	syscall				# printf("n = ");
+	syscall				# printf("n  = ");
 
 	li	$v0, 5			# syscall 5: read_int
-	syscall				# 
+	syscall				#
 	move	$t0, $v0		# scanf("%d", &n);
 
-	# TODO: implement a loop
+	li	$t1, 1			# int fac = 1;
 
+loop_init:
+	li	$t2, 1			# int i = 1;
+
+loop_cond:
+	bgt	$t2, $t0, loop_end
+
+loop_body:
+	mul	$t1, $t1, $t2		# fac = fac * i;
+	addi	$t2, $t2, 1		# i++;
+	
+	j	loop_cond		# goto loop_cond;
+
+loop_end:
 	li	$v0, 4			# syscall 4: print_string
 	la	$a0, result_msg		#
-	syscall				# printf("n! = ");
+	syscall				# printf("n!  = ");
 
-	# TODO: print the result
 
+	li	$v0, 1			# syscall 1: print_int
+	move	$a0, $t1		#
+	syscall				# printf("%d", fac);
+	
 	li	$v0, 11			# syscall 11: print_char
 	li	$a0, '\n'		#
-	syscall				# putchar('\n');
+	syscall				# putchar('\n')
 
-	li	$v0, 0
+	li	$v0, 0			
 	jr	$ra			# return 0;
+
+
 
 	.data
 prompt_msg:
-	.asciiz	"n = "
+	.asciiz	"n  = "
 result_msg:
-	.asciiz	"n! = "
+	.asciiz "n! = "
